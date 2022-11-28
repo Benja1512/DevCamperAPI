c69e59b7ae9f929d9195d29c97928972a0cb3d5a
benjamin <bontiveros15@gmail.com>
2022-11-28 16:17:22 -0500

    16.17 time actualizado

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db')
 
 
@@ -29,4 +30,12 @@ app.use('/api/v1/bootcamps', bootcamps);
 const PORT = process.env.PORT || 5000;
 
 app.listen(
-    PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+    PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+
+//Handle unhandled promise rejections
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    //close server & exit process
+    server.close(() => process.exit(1));
+});
